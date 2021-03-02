d8858c62dd6aae287092f5271e054ad1dadb231f
Ritsuki KOKUBO <rpaka@outlook.jp>
2021-03-02 14:41:47 +0900

    アクセサリ単体へ

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform'; 
+import { ExamplePlatformAccessory } from './platformAccessory';
 
-/**
- * This method registers the platform with Homebridge
- */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerAccessory('Aircon', ExamplePlatformAccessory);
 };
